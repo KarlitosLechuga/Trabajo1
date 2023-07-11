8c71b2e09cac222923e7739c201b6e71c46ca5af
Lukas Urrutia <lu.urrutia@duocuc.cl>
2023-07-11 01:54:11 -0400

    delete

--- TiendaArmas/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Categoria,Productos
-# Register your models here.
-
-# ACCESO A LAS TABLAS CREADAS EN CORE/MODELS
-
-admin.site.register(Categoria)
-admin.site.register(Productos)
